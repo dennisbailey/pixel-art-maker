62affc16c6709c8796e07b0e9ac8f429434d682a
Dennis Bailey <bailey.dennis@gmail.com>
2016-01-14 21:10:45 -0700

    One div will change color on click

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("READY PALYER ONE?");
+
+addEventListener("click", function() {
+    document.getElementById('clickMe').style.backgroundColor = "red";
+});
+
+
+
+document.getElementById('clickMe').onclick = red;
